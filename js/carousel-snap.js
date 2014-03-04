83d3bb576c768a90446518bdf7fcd2bebbf776c8
Jes Anub <mjanub@gmail.com>
2014-03-05 07:21:51 +0800

    Set up container layout

--- js/carousel-snap.js
+++ js/carousel-snap.js
@@ -8,6 +8,17 @@
 ( function( $ ) {
 	var CarouselSnap = function( element, options ) {
 		var settings = $.extend( {}, $.fn.carouselSnap.defaults, options );
+		var container = $( element );
+		var containerLength = container.children().length * container.children().outerWidth( true );
+		var setContainerWidth = function() {
+			container.css( 'width', containerLength );
+		};
+
+		var initialize = function () {
+			setContainerWidth();
+		}
+
+		initialize();
 
 	};
 
--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 ( function( $ ){
-
+	$('.vid-tab').carouselSnap();
 })( jQuery );
