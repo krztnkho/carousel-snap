b4463f87ecbd7878acb5e94bd27e522f1890da06
Kristine Kho <krztnkho@gmail.com>
2014-03-05 12:58:09 +0800

    Time Integrated with prev and next button.

--- js/carousel-snap.js
+++ js/carousel-snap.js
@@ -21,46 +21,77 @@
 		var setContainerWidth = function() {
 			container.css( 'width', containerLength );
 		};
-		var appendPrevNextButtons = function () {
-			container.after('<div class="prevNext prevLink" id="' + settings.prevID + '">Previous</div><div class="prevNext nextLink" id="' + settings.nextID + '">Next</div>');
-		}
+		var appendPrevNextButtons = function() {
+			container.after( '<div class="prevNext prevLink" id="' + settings.prevID + '">Previous</div><div class="prevNext nextLink" id="' + settings.nextID + '">Next</div>' );
+		};
 
-		var initializeSettings = function () {
-			itemLength = 	container.children().length;
+		var initializeSettings = function() {
+			itemLength = container.children().length;
 			console.log( 'itemLength ' + itemLength );
-			container.children().css('left', 'auto');
+			container.children().css( 'left', 'auto' );
 			currentIndex = 0;
 			remainingPanes = ( itemLength / visibleItems ) - 1;
-		}
+		};
 
-		var listenToClick = function () {
+		var listenToClick = function() {
 			$( '#' + settings.nextID ).click( function() {
-				if(currentIndex < remainingPanes ){
+				if ( currentIndex < remainingPanes ) {
 					console.log( 'here' );
-					console.log(	'currentIndex next' + currentIndex );
+					console.log( 'currentIndex next' + currentIndex );
 					console.log( 'remainingPanes ' + remainingPanes );
 					currentIndex++;
-					container.children().animate( { 'left': moveby } );
-	      }
-			})
+					container.children().animate( {
+						'left': moveby
+					} );
+				}
+			} );
 			$( '#' + settings.prevID ).click( function() {
-				if( currentIndex > 0 ){
-	        currentIndex--;
-	        console.log( 'currentIndex' + currentIndex );
-	        container.children().animate( { 'left': movebyPrev } );
-	      }
-			})
-		}
+				if ( currentIndex > 0 ) {
+					currentIndex--;
+					console.log( 'currentIndex' + currentIndex );
+					container.children().animate( {
+						'left': movebyPrev
+					} );
+				}
+			} );
+		};
 
-		var initialize = function () {
+		var listenToHover = function() {
+			$.fn.carouselSnap.checkHover( '#' + settings.nextID, function() {
+				if ( currentIndex < remainingPanes ) {
+					console.log( 'here' );
+					console.log( 'currentIndex next' + currentIndex );
+					console.log( 'remainingPanes ' + remainingPanes );
+					currentIndex++;
+					container.children().animate( {
+						'left': moveby
+					} );
+				}
+			} );
+
+			$.fn.carouselSnap.checkHover( '#' + settings.prevID , function() {
+				if ( currentIndex > 0 ) {
+					currentIndex--;
+					console.log( 'currentIndex' + currentIndex );
+					container.children().animate( {
+						'left': movebyPrev
+					} );
+				}
+			} );
+		};
+
+		var initialize = function() {
 			appendPrevNextButtons();
 			setContainerWidth();
 			initializeSettings();
 			listenToClick();
-		}
+			listenToHover();
+		};
 		initialize();
 
 	};
+
+
 
 	$.fn.carouselSnap = function( options ) {
 		return this.each( function( key, value ) {
@@ -73,34 +104,39 @@
 		} );
 	};
 
-    /**
-     * function to get time on how long did the user hover on the element.
-     * To use
-     * checkHover( '#theelement' , function( hoverTime){
-     *   code to do with the time;
-     * } )
-     * @param  html element The element to check.
-     * @param  function callback  Called when function is done.
-     * @return Object hoverTime The time it is hovering on the element in seconds.
-     */
+	/**
+	 * function to get time on how long did the user hover on the element.
+	 * To use
+	 * checkHover( '#theelement' , function( hoverTime){
+	 *   code to do with the time;
+	 * } )
+	 * @param  html element The element to check.
+	 * @param  function callback  Called when function is done.
+	 * @return Object hoverTime The time it is hovering on the element in seconds.
+	 */
+	var timeoutId;
 	$.fn.carouselSnap.checkHover = function( element, callback ) {
-		$( element ).hover( function( ) {
-			//get start time
-			$( this ).data( 'onHover', new Date().getTime() );
+		$( element ).hover( function() {
+			//start time
+			if ( !timeoutId ) {
+				timeoutId = window.setInterval( function() {
+					callback();
+				}, $.fn.carouselSnap.defaults.time );
+			}
 		}, function() {
-			//get end time
-			var outHover = new Date().getTime();
-			//time is start
-			var hoverTime = ( outHover - $( this ).data( 'onHover' ) ) / 1000;
-			//return time in seconds
-			callback( hoverTime );
+			//reset time
+			if ( timeoutId ) {
+				window.clearInterval( timeoutId );
+				timeoutId = null;
+			}
 		} );
 	};
 
 	$.fn.carouselSnap.defaults = {
 		nextID: 'next-slide',
 		prevID: 'previous-slide',
-		elementsToMove: 3
+		elementsToMove: 3,
+		time: 2000
 	};
 
 } )( jQuery );
