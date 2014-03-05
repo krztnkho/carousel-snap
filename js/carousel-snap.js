27814444f944bc7eb0cb4ee9ec542e1e626fbf1b
Jes Anub <mjanub@gmail.com>
2014-03-05 13:17:13 +0800

    Set variables for computation

--- js/carousel-snap.js
+++ js/carousel-snap.js
@@ -8,79 +8,93 @@
 	var CarouselSnap = function( element, options ) {
 
 		var settings = $.extend( {}, $.fn.carouselSnap.defaults, options );
-		var currentIndex = 0;
-		var container = $( element );
-		var itemLength;
+
+		var currentPane  = 0;
+		var domReady     = true;
+		var container    = $( element );
 		var visibleItems = settings.elementsToMove;
-		var remainingPanes;
+
+		var itemLength;
+		var containerLength;
+		var currentTotalPanes;
+		var paneToFetch;
+		var totalPanes;
+
 		var moveby = '-=' + ( container.children().outerWidth( true ) * visibleItems ) + 'px';
 		var movebyPrev = '+=' + ( container.children().outerWidth( true ) * visibleItems ) + 'px';
 
-		var containerLength = container.children().length * container.children().outerWidth( true );
-
 		var setContainerWidth = function() {
+			containerLength = container.children().length * container.children().outerWidth( true );
 			container.css( 'width', containerLength );
 		};
 		var appendPrevNextButtons = function() {
 			container.after( '<div class="prevNext prevLink" id="' + settings.prevID + '">Previous</div><div class="prevNext nextLink" id="' + settings.nextID + '">Next</div>' );
 		};
 
-		var initializeSettings = function() {
-			itemLength = container.children().length;
-			console.log( 'itemLength ' + itemLength );
-			container.children().css( 'left', 'auto' );
-			currentIndex = 0;
-			remainingPanes = ( itemLength / visibleItems ) - 1;
-		};
-
-		var listenToClick = function() {
-			$( '#' + settings.nextID ).click( function() {
-				if ( currentIndex < remainingPanes ) {
-					console.log( 'here' );
-					console.log( 'currentIndex next' + currentIndex );
-					console.log( 'remainingPanes ' + remainingPanes );
-					currentIndex++;
-					container.children().animate( {
-						'left': moveby
-					} );
-				}
-			} );
-			$( '#' + settings.prevID ).click( function() {
-				if ( currentIndex > 0 ) {
-					currentIndex--;
-					console.log( 'currentIndex' + currentIndex );
-					container.children().animate( {
-						'left': movebyPrev
-					} );
-				}
-			} );
-		};
-
 		var listenToHover = function() {
-			$.fn.carouselSnap.checkHover( '#' + settings.nextID, function() {
-				if ( currentIndex < remainingPanes ) {
-					console.log( 'here' );
-					console.log( 'currentIndex next' + currentIndex );
-					console.log( 'remainingPanes ' + remainingPanes );
-					currentIndex++;
-					container.children().animate( {
-						'left': moveby
-					} );
-				}
-			} );
+			// $.fn.carouselSnap.checkHover( '#' + settings.nextID, function() {
+			// 	if ( currentIndex < remainingPanes ) {
+			// 		console.log( 'here' );
+			// 		console.log( 'currentIndex next' + currentIndex );
+			// 		console.log( 'remainingPanes ' + remainingPanes );
+			// 		currentIndex++;
+			// 		container.children().animate( {
+			// 			'left': moveby
+			// 		} );
+			// 	}
+			// } );
 
-			$.fn.carouselSnap.checkHover( '#' + settings.prevID , function() {
-				if ( currentIndex > 0 ) {
-					currentIndex--;
-					console.log( 'currentIndex' + currentIndex );
-					container.children().animate( {
-						'left': movebyPrev
-					} );
-				}
-			} );
+			// $.fn.carouselSnap.checkHover( '#' + settings.prevID , function() {
+			// 	if ( currentIndex > 0 ) {
+			// 		currentIndex--;
+			// 		console.log( 'currentIndex' + currentIndex );
+			// 		container.children().animate( {
+			// 			'left': movebyPrev
+			// 		} );
+			// 	}
+			// } );
 		};
 
-		var initialize = function() {
+		var initializeSettings = function () {
+			itemLength = 	container.children().length;
+			paneToFetch = ( itemLength / visibleItems );
+			currentTotalPanes = paneToFetch - 1;
+			totalPanes = Math.ceil( ( settings.totalItems / visibleItems ) );
+		}
+
+		var fetchOnFirstPane = function () {
+			settings.onFirstScroll( function( domCheck ) {
+				domReady = domCheck;
+			} );
+			setContainerWidth();
+			initializeSettings();
+		}
+
+		var listenToClick = function () {
+			console.log(	'totalPanes: ' + totalPanes );
+			console.log(	'currentPane: ' + (currentPane + 1) );
+			console.log( 'currentTotalPanes: ' + (currentTotalPanes + 1) );
+			$( '#' + settings.nextID ).click( function() {
+				if( (currentPane < currentTotalPanes) && domReady ){
+					if ( !currentPane ) {
+						fetchOnFirstPane();
+					}
+					currentPane++;
+					console.log(	'currentPane: ' + currentPane );
+					console.log( 'currentTotalPanes: ' + ( currentTotalPanes + 1 ) );
+					container.children().animate( { 'left': moveby } );
+	      }
+			})
+			$( '#' + settings.prevID ).click( function() {
+				if( currentPane > 0 ){
+	        currentPane--;
+	        console.log( 'currentPane: ' + (currentPane + 1) );
+	        container.children().animate( { 'left': movebyPrev } );
+	      }
+			})
+		}
+
+		var initialize = function () {
 			appendPrevNextButtons();
 			setContainerWidth();
 			initializeSettings();
@@ -136,7 +150,12 @@
 		nextID: 'next-slide',
 		prevID: 'previous-slide',
 		elementsToMove: 3,
-		time: 2000
+		time: 2000,
+		loadPerFetch: 10,
+		onFirstScroll: function( callback ) {
+			callback( true );
+		},
+		totalItems: 60
 	};
 
 } )( jQuery );
--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 ( function( $ ){
+
+	var colorArrays = [ 'black', 'red', 'green', 'blue' ];
+	var color = 0;
+	var loadMoreElements = function( callback ) {
+		for (var i = 1; i <= 4; i++) {
+			$('.vid-tab').append('<li class="dummy ' + colorArrays[ color ] + '"></li>');
+		};
+		if( color < colorArrays.length ) {
+			color++;
+		} else {
+			color = 0;
+		}
+		callback( 1 );
+	}
+
 	$('.vid-tab').carouselSnap({
 		nextID: 'nextSlide',
 		prevID: 'previousSlide',
-		elementsToMove: 4
+		elementsToMove: 4,
+		onFirstScroll: loadMoreElements,
+		loadPerFetch: 8,
+		totalItems: 12
 	});
 })( jQuery );
