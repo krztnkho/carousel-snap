c88c457ea43ab4db0ad185c2926875c0f4da65a5
Kristine Kho <krztnkho@gmail.com>
2014-03-20 11:12:20 +0800

    Additional Changes: 	> Added git ignore file. 	> Added Hover Functionality. 	> Fix colliding execution of click and hover. 	> Added different shift movements on click and hover.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
-( function( $ ){
+( function( $ ) {
 
-	var colorArrays     = [ 'black', 'red', 'green', 'blue' ];
-	var color           = 0;
+	var colorArrays = [ 'black', 'red', 'green', 'blue' ];
+	var color = 0;
 	var fetchMultiplier = 0;
 
 	/* Loading effect */
 	var loadingEffect = {
-		'show' :	function () {
-								var x = 0;
-								$( 'body' ).append( '<div id="loading"></div>' )
-								setInterval( function () {
-									x = ++x % 4;
-									$( '#loading' ).html( 'loading' + Array( x + 1 ).join( '.' ) );
-								}, 300 );
-							},
-		'hide' :	function () {
-								$( '#loading' ).remove();
-							}
+		'show': function() {
+			var x = 0;
+			$( 'body' ).append( '<div id="loading"></div>' )
+			setInterval( function() {
+				x = ++x % 4;
+				$( '#loading' ).html( 'loading' + Array( x + 1 ).join( '.' ) );
+			}, 300 );
+		},
+		'hide': function() {
+			$( '#loading' ).remove();
+		}
 	}
 
-	var loadElements = function ( color, callback ) {
+	var loadElements = function( color, callback ) {
 
 		/* Unbind trigger to API Call event */
 		$( '#appendNow' ).off( 'click', loadMoreElements );
@@ -31,8 +31,8 @@
 		var items = '';
 
 		/* API Call Simulation */
-		setTimeout( function () {
-			for( var j = 0; j < 4; j++ ) {
+		setTimeout( function() {
+			for ( var j = 0; j < 4; j++ ) {
 				for ( var i = 1; i <= 6; i++ ) {
 					var countItem = i + ( j * 6 ) + ( fetchMultiplier * 24 );
 					items = items + '<li class="dummy ' + color + '"><div>' + countItem + '</div></li>';
@@ -45,19 +45,24 @@
 	}
 
 	loadElements( 0, function( items ) {
-			$('.vid-tab').append( items );
-			loadingEffect.hide();
-			$( '.vid-tab' ).carouselSnap( {
-				elementsToMove: 4
-			} );
+		$( '.vid-tab' ).append( items );
+		loadingEffect.hide();
+		$( '.vid-tab' ).carouselSnap( {
+			nextID: 'next-slide',
+			prevID: 'previous-slide',
+			elementsToMove: 4,
+			startOnCenter: false,
+			time: 1500
+
+		} );
 	} )
 
-	var loadMoreElements = function () {
+	var loadMoreElements = function() {
 		loadElements( colorArrays[ color++ ], function( items ) {
-			$('.vid-tab').carouselSnap.appendItems( items )
+			$( '.vid-tab' ).carouselSnap.appendItems( items )
 			loadingEffect.hide();
 		} );
 		color = ( color > 3 ) ? 0 : color;
 	}
 
-})( jQuery );
+} )( jQuery );
